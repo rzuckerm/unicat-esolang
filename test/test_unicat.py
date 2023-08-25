76b286fc1a3f149f934e77a339fa6d0878f5bdb5
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-08-24 20:08:56 -0500

    Add tests for number parsing

--- test/test_unicat.py
+++ test/test_unicat.py
@@ -1,12 +1,30 @@
 from unittest.mock import patch
 import sys
 
-from unicat_esolang.unicat import main
+import pytest
+
+from unicat_esolang import unicat
+
+
+@pytest.mark.parametrize(
+    "byte_code,expected_number",
+    [
+        ("", 1337),
+        ("1", 1337),
+        ("88", 0),
+        ("87", 0),
+        ("1234567088", 0o12345670),
+        ("7654321087", -0o76543210),
+    ],
+)
+def test_parse_number(byte_code, expected_number):
+    number = unicat.parse_number(iter(byte_code))
+    assert number == expected_number
 
 
 def test_hello_world(capsys):
     with patch.object(sys, "argv", ["unicat", "examples/hello-world.cat"]):
-        main()
+        unicat.main()
 
     output = capsys.readouterr().out
     assert output == "Hello, World!\n"
